12ee1a888bf496e0cce65678ae7a4ace37ac9113
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-03 13:22:00 +0300

    completed react-router-routes-lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  let directorsCards = directors.map(director => (
+    <div key={director.name}>
+      Name: {director.name}
+      Movies:
+      <ul>
+        {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsCards}
+    </div>);
 }
 
 export default Directors;
